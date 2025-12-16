8ad2e6d47cab336af53dbddd7cc31b50841918ec
Kima09 <hkdavidl666@gmail.com>
2025-12-16 19:45:36 +0900

    (2025.12.16) 유저 인터페이스

---
+++ Program/Assets/Class5th (User Interface)/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Execute()
+    {
+        Debug.Log("Start");
+    }
+    
+    public void Options()
+    {
+        Debug.Log("Options");
+    }
+    
+    public void Exit()
+    {
+        Debug.Log("Exit");
+    }
+}
