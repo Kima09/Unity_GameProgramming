a36f4665b362fe2ac432b0b671722594edbab779
Kima09 <hkdavidl666@gmail.com>
2025-12-22 19:45:51 +0900

    (2025.12.22) 입력 시스템

---
+++ Program/Assets/Class9th (Input System)/Scripts/Character.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Vector3 direction;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+
+        direction.z = Input.GetAxisRaw("Vertical");
+        
+        transform.position = transform.position + direction*speed*Time.deltaTime;
+    }
+}
